d9d025463dda9ca545f1927aa8ee305084b6cdf8
Jonathan Hong <jonathan.hong8@gmail.com>
2014-07-06 16:58:18 -0700

    altered view all bikes and users tables

--- bikeshare.py
+++ bikeshare.py
@@ -252,7 +252,7 @@ def internal_server_error(e):
 
 if __name__ == '__main__':
     if debug:
-        app.run(host='127.0.0.1', port=8081, debug=True)
+        app.run(host='127.0.0.1', port=8082, debug=True)
     else:
-        app.run(host='0.0.0.0', port=8081, debug=True)
+        app.run(host='0.0.0.0', port=8082, debug=True)
 
